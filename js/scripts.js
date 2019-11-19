0e64008877486996d6abffa80481222a237e67a2
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-11-18 21:45:05 -0600

    remove last name input field and variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     var firstNameInput = $("input#firstName").val();
-    var lastNameInput = $("input#lastName").val();
 
     $(".firstName").text(firstNameInput);
-    $(".lastName").text(lastNameInput);
 
     $("#letter").show();
 
